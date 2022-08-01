a4406aa990d9045c84a597ddd0d7289a6fb9cb97
jackinquantico <104432549+jackinquantico@users.noreply.github.com>
2022-08-01 18:58:39 +0900

    initial commit

---
+++ src/com/kh/part_02/VariableStudy.java
@@ -0,0 +1,12 @@
+package com.kh.part_02;
+
+public class VariableStudy {
+
+	// 기본형 8가지 + 참조형 1가지 변수 선언 및 초기화하여 출력하는 메소드
+	public void printVariable() {
+		int i = 1;
+		
+		System.out.println(i);
+	}
+	
+}
---
+++ src/com/kh/part_02/run/RunVariableStudy.java
@@ -0,0 +1,11 @@
+package com.kh.part_02.run;
+
+import com.kh.part_02.VariableStudy;
+
+public class RunVariableStudy {
+	public static void main(String[] args) {
+		VariableStudy vs = new VariableStudy();
+		
+		vs.printVariable();
+	}
+}
