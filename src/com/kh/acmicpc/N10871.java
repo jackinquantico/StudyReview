2c1d11f5edcbe246ba39ede470ae031dc3966b40
jackinquantico <104432549+jackinquantico@users.noreply.github.com>
2022-08-04 14:09:24 +0900

    baekjoon problem 10871

---
+++ src/com/kh/acmicpc/N10871.java
@@ -0,0 +1,26 @@
+package com.kh.acmicpc;
+
+import java.util.Scanner;
+
+public class N10871 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        sc.nextLine();
+        
+        int[] a = new int[n];
+    
+        for (int i=0; i<a.length; i++) {
+        	a[i]= sc.nextInt();
+        }
+        
+        for (int i=0; i<a.length; i++){
+            if(a[i] < x){
+                System.out.print(a[i] + " ");
+            }
+        }
+	}
+
+}
