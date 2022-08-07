4726e94aaf78d6b167f1ae324fdefa326b208099
jackinquantico <jackinquantico@gmail.com>
2022-08-07 13:18:36 +0900

    baekjoon problem 10809

---
+++ src/com/kh/acmicpc/N10809.java
@@ -0,0 +1,30 @@
+package com.kh.acmicpc;
+
+import java.util.Scanner;
+
+public class N10809 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		int[] at = new int[26];
+		
+		for (int i=0; i<at.length; i++) {
+			at[i] = -1; 
+		}
+		
+		for (int i=0; i<input.length(); i++) {
+			char ch = input.charAt(i);
+			
+			if (at[ch-'a'] == -1) {
+				at[ch-'a'] = i;
+			}
+		}
+		
+		for (int i=0; i<at.length; i++) {
+			System.out.print(at[i] + " ");
+		}
+	}
+}
