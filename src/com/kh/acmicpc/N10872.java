701104fc8c4e4444a3b5827c41fac110b15eeb43
jackinquantico <jackinquantico@naver.com>
2022-08-07 15:30:57 +0900

    baekjoon problem 10872

---
+++ src/com/kh/acmicpc/N10872.java
@@ -0,0 +1,22 @@
+package com.kh.acmicpc;
+
+import java.util.Scanner;
+
+public class N10872 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum = 1;
+		
+		for (int i=1; i<=n; i++) {
+			sum *= i;
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
