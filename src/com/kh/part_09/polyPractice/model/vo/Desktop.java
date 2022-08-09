70d1528bff8d526cfd42af430a4a2a1b03973cbc
jackinquantico <jackinquantico@naver.com>
2022-08-09 16:16:35 +0900

    about inheritance

---
+++ src/com/kh/part_09/polyPractice/model/vo/Desktop.java
@@ -0,0 +1,29 @@
+package com.kh.part_09.polyPractice.model.vo;
+
+public class Desktop extends Electronic {
+
+	public static final String CPU = "intel";
+	private String graphic;
+	
+	public Desktop() {
+		
+	}
+	
+	public Desktop(String brand, String name, int price, String graphic) {
+		super(brand, name, price);
+		this.graphic = graphic;
+	}
+
+	public String getGraphic() {
+		return graphic;
+	}
+
+	public void setGraphic(String graphic) {
+		this.graphic = graphic;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + ", CPU : "+CPU+", graphic : "+graphic;
+	}
+}
