0f532cb15c6bcbe89c2ed27d9a652065b5962da9
jackinquantico <jackinquantico@naver.com>
2022-08-12 13:57:13 +0900

    baekjoon problem solved

---
+++ src/com/kh/acmicpc/N15552.java
@@ -0,0 +1,33 @@
+package com.kh.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 빠른 입출력
+public class N15552 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long t = Long.parseLong(br.readLine());
+		
+		StringTokenizer st;
+		
+		for (int i=0; i<t; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+"\n");
+		}
+		br.close();
+		
+		bw.flush();
+		bw.close();		
+
+	}
+
+}
---
+++ src/com/kh/acmicpc/N15596.java
@@ -0,0 +1,23 @@
+package com.kh.acmicpc;
+
+public class N15596 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+}
+
+class Test {
+	
+	public long test(int[] a) {
+		long sum = 0;
+		
+		for (int i=0; i<a.length; i++) {
+			sum += a[i];
+		}
+		
+		return sum;
+	}
+}
