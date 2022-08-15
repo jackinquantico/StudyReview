e0e2b3af868007676b4d8b3d2f34b5817feda1a9
jackinquantico <jackinquantico@naver.com>
2022-08-15 21:46:56 +0900

    baekjoon problem solved

--- src/com/kh/acmicpc/N2941.java
+++ src/com/kh/acmicpc/N2941.java
@@ -17,42 +17,47 @@ public class N2941 {
 			char ch = str.charAt(i);
 			
 			if (ch == 'c') {
-				if (str.charAt(i+1) == '=') {
-					i++;
-				} else if (str.charAt(i+1) == '-') {
-					i++;
+				if (i < str.length()-1) {
+					if (str.charAt(i+1) == '=') {
+						i++;
+					} else if (str.charAt(i+1) == '-') {
+						i++;
+					}
 				}
-			}
-			
-			if (ch == 'd') {
-				if (str.charAt(i+1) == '-') {
-					i++;
-				} else if (str.charAt(i+2) == '=') {
-					i += 2;
+			} else if (ch == 'd') {
+				if (i < str.length()-1) {
+					if (str.charAt(i+1) == 'z') {
+						if (i < str.length() - 2)
+							if (str.charAt(i+2) == '=')	 {
+								i += 2;	
+							}
+					} else if (str.charAt(i+1) == '-') {
+						i++;
+					}
 				}
-			}
-			
-			if (ch == 'l') {
-				if (str.charAt(i+1) == 'j') {
-					i++;
+			} else if (ch == 'l') {
+				if (i < str.length()-1) {
+					if (str.charAt(i+1) == 'j') {
+						i++;
+					}
 				}
-			}
-			
-			if (ch == 'n') {
-				if (str.charAt(i+1) == 'j') {
-					i++;
+			} else if (ch == 'n') {
+				if (i < str.length()-1) {
+					if (str.charAt(i+1) == 'j') {
+						i++;
+					}
 				}
-			}
-			
-			if (ch == 's') {
-				if (str.charAt(i+1) == '=') {
-					i++;
+			} else if (ch == 's') {
+				if (i < str.length()-1) {
+					if (str.charAt(i+1) == '=') {
+						i++;
+					}
 				}
-			}
-			
-			if (ch == 'z') {
-				if (str.charAt(i+1) == '=') {
-					i++;
+			} else if (ch == 'z') {
+				if (i < str.length()-1) {
+					if (str.charAt(i+1) == '=') {
+						i++;
+					}
 				}
 			}
 			
