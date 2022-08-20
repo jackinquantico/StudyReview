4261ea75f74f4d562b5c39db5f8c9437a9235d3d
jackinquantico <jackinquantico@naver.com>
2022-08-20 16:07:34 +0900

    music manage program - run class

---
+++ src/com/kh/part_14/musicMVC/run/MusicRun.java
@@ -0,0 +1,14 @@
+package com.kh.part_14.musicMVC.run;
+
+import com.kh.part_14.musicMVC.view.MusicView;
+
+public class MusicRun {
+
+	public static void main(String[] args) {
+		
+		MusicView mv = new MusicView();
+		
+		mv.mainMenu();
+	}
+
+}
