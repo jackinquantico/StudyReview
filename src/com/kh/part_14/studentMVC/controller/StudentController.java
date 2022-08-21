f556795cf52c499995d993b2fde51589c9355ce2
jackinquantico <jackinquantico@naver.com>
2022-08-21 20:10:16 +0900

    implements other methods

---
+++ src/com/kh/part_14/studentMVC/controller/StudentController.java
@@ -0,0 +1,82 @@
+package com.kh.part_14.studentMVC.controller;
+
+import java.util.ArrayList;
+
+import com.kh.part_14.studentMVC.model.vo.Student;
+
+public class StudentController {
+
+	ArrayList<Student> list = new ArrayList<Student>();
+	
+	public int insertStudent(String name, int age, int kor, int eng, int math) {
+		
+		int result = list.size();
+		
+		list.add(new Student(name, age, kor, eng, math));
+		
+		return list.size() - result;
+	}
+	
+	public ArrayList<Student> selectStudentList() {
+		
+		return list;
+	}
+	
+	public int updateStudent(String name, String upName, int upAge, int upKor, int upEng, int upMath) {
+		
+		int result = 0;
+		
+		for (int i=0; i<list.size(); i++) {
+			if (list.get(i).getName().equals(name)) {
+				list.get(i).setName(upName);
+				list.get(i).setAge(upAge);
+				list.get(i).setKor(upKor);
+				list.get(i).setEng(upEng);
+				list.get(i).setMath(upMath);
+				result++;
+			}
+		}
+		
+		return result;
+	}
+	
+	public int deleteStudent(String name) {
+		
+		int result = 0;
+		
+		for (int i=0; i<list.size(); i++) {
+			if (list.get(i).getName().equals(name)) {
+				list.remove(i);
+				result++;
+			}
+		}
+		
+		return result;
+	}
+	
+	public ArrayList<Student> searchStudent(String keyword) {
+		
+		ArrayList<Student> searched = new ArrayList<Student>();
+		
+		for (int i=0; i<list.size(); i++) {
+			if (list.get(i).getName().contains(keyword)) {
+				searched.add(list.get(i));
+			}
+		}
+		
+		return searched;
+	}
+	
+	public double averageKor() {
+		
+		int sum = 0;
+		
+		for (int i=0; i<list.size(); i++) {
+			sum += list.get(i).getKor();
+		}
+		
+		
+		return (double)sum / list.size();
+	}
+	
+}
--- src/com/kh/part_14/studentMVC/view/StudentView.java
+++ src/com/kh/part_14/studentMVC/view/StudentView.java
@@ -1,8 +1,10 @@
 package com.kh.part_14.studentMVC.view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.kh.part_14.studentMVC.controller.StudentController;
+import com.kh.part_14.studentMVC.model.vo.Student;
 
 public class StudentView {
 
@@ -58,26 +60,146 @@ public class StudentView {
 	
 	public void insertStudent() {
 		
+		System.out.println("=== 학생 정보 추가 ===");
+		
+		// 학생 이름, 나이, 국어, 영어, 수학 점수 입력
+		System.out.print("학생 이름 입력 : ");
+		String name = sc.nextLine();
+		System.out.print("학생 나이 입력 : ");
+		int age = sc.nextInt();
+		sc.nextLine();
+		System.out.print("국어 점수 입력 : ");
+		int kor = sc.nextInt();
+		sc.nextLine();
+		System.out.print("영어 점수 입력 : ");
+		int eng = sc.nextInt();
+		sc.nextLine();
+		System.out.print("수학 점수 입력  : ");
+		int math = sc.nextInt();
+		sc.nextLine();
+		
+		// insertStudent() 매개변수로 전달
+		// 결과물 result 반환
+		int result = scontrol.insertStudent(name, age, kor, eng, math);
+		
+		// 양수면 성공, 0이면 실패 출력
+		if (result > 0) {
+			System.out.println("성공적으로 추가되었습니다.");
+		} else {
+			System.out.println("추가에 실패했습니다.");
+		}
+		
 	}
 	
 	public void selectStudentList() {
+		
+		System.out.println("=== 학생 정보 조회 ===");
+		
+		// selectStudentList() 호출
+		// 결과물 list 반환
+		ArrayList<Student> list = scontrol.selectStudentList();
+		
+		// list가 비어있으면 실패, list가 비어있지 않으면 for문으로 출력
+		if (list.isEmpty()) {
+			System.out.println("현재 학생 목록이 비어있습니다.");
+		} else {
+			for (Student s : list) {
+				System.out.println(s);
+			}
+		}
 		
 	}
 	
 	public void updateStudent() {
 		
+		System.out.println("=== 학생 정보 수정 ===");
+		
+		// 수정할 학생 이름 입력
+		// 수정내용 입력받기
+		System.out.print("수정할 학생 이름 입력 : ");
+		String name = sc.nextLine();
+		System.out.print("수정내용 입력(이름) : ");
+		String upName = sc.nextLine();
+		System.out.print("수정내용 입력(나이) : ");
+		int upAge = sc.nextInt();
+		sc.nextLine();
+		System.out.print("수정내용 입력(국어 점수) : ");
+		int upKor = sc.nextInt();
+		sc.nextLine();
+		System.out.print("수정내용 입력(영어 점수) : ");
+		int upEng = sc.nextInt();
+		sc.nextLine();
+		System.out.print("수정내용 입력(수학 점수) : ");
+		int upMath = sc.nextInt();
+		sc.nextLine();		
+		
+		// updateStudnt() 호출
+		// 결과물 result 반환
+		int result = scontrol.updateStudent(name, upName, upAge, upKor, upEng, upMath);
+		
+		// result가 0이면 실패, 양수면 성공 출력
+		if (result > 0) {
+			System.out.println("정보 수정에 성공했습니다.");
+		} else {
+			System.out.println("수정할 학생 정보가 존재하지 않습니다.");
+		}
+		
 	}
 	
 	public void deleteStudent() {
 		
+		System.out.println("=== 학생 정보 삭제 ===");
+		
+		// 삭제할 학생 이름 입력
+		System.out.print("삭제할 학생 이름 입력 : ");
+		String name = sc.nextLine();
+		
+		// deleteStudent() 호출
+		// 결과물 result 반환
+		int result = scontrol.deleteStudent(name);
+		
+		// result 가 0이면 실패, 양수면 성공 출력
+		if (result > 0) {
+			System.out.println("정보 삭제에 성공했습니다.");
+		} else {
+			System.out.println("삭제할 학생 정보가 존재하지 않습니다.");
+		}
+				
 	}
 	
 	public void searchStudent() {
+		
+		System.out.println("=== 학생 정보 검색 ===");
+		
+		// 검색할 키워드 입력
+		System.out.print("검색할 학생 이름 키워드 입력 : ");
+		String keyword = sc.nextLine();
+		
+		// searchStudent() 호출
+		// 결과물 searched 반환
+		ArrayList<Student> searched = scontrol.searchStudent(keyword);
+		
+		// list가 비어있으면 실패, 비어있지 않으면 for문으로 출력
+		if (searched.isEmpty()) {
+			System.out.println("해당 학생이 존재하지 않습니다.");
+		} else {
+			for (Student s : searched) {
+				System.out.println(s);
+			}
+		}
 		
 	}
 	
 	public void averageKor() {
 		
+		System.out.println("=== 국어 점수 평균 ===");
+		
+		// averageKor() 호출
+		// 결과물 average 반환
+		double average = scontrol.averageKor();
+		
+		// average 출력
+		System.out.println("국어 점수 평군 : "+average);
 	}
 	
 }
