b4c5f0a7ded9ee6d8a7934a526d584b2bc1a7118
jackinquantico <jackinquantico@naver.com>
2022-08-22 16:50:20 +0900

    movie reservation program - run class - intial commit

---
+++ src/com/kh/part_14/movieReservation/run/MovieRun.java
@@ -0,0 +1,15 @@
+package com.kh.part_14.movieReservation.run;
+
+import com.kh.part_14.movieReservation.view.MovieView;
+
+public class MovieRun {
+
+	public static void main(String[] args) {
+		
+		MovieView mv = new MovieView();
+		
+		mv.mainMenu();
+
+	}
+
+}
