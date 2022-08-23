f6e7cb1b51865c55cdb77828e7901a1bdede772c
jackinquantico <jackinquantico@naver.com>
2022-08-23 09:39:13 +0900

    parking tower program - run class

---
+++ src/com/kh/part_14/ParkingTowerMVC/run/Run.java
@@ -0,0 +1,15 @@
+package com.kh.part_14.ParkingTowerMVC.run;
+
+import com.kh.part_14.ParkingTowerMVC.view.ParkingTowerView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		ParkingTowerView ptv = new ParkingTowerView();
+		
+		ptv.mainMenu();
+
+	}
+
+}
