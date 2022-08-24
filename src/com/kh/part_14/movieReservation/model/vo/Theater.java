19aac5fc92ee37eafd9fdff3073f16a70b7f95dd
jackinquantico <jackinquantico@naver.com>
2022-08-24 09:07:23 +0900

    movie reservation program - add id/pw system

---
+++ src/com/kh/part_14/movieReservation/model/vo/Theater.java
@@ -0,0 +1,46 @@
+package com.kh.part_14.movieReservation.model.vo;
+
+public class Theater {
+	
+	// ±ØÀå Á÷¿ø id, pw
+	private String sysId = "sysid";
+	private String sysPw = "syspw";
+	
+	// ¼Õ´Ô id, pw
+	private String cosId;
+	private String cosPw;
+	
+	public Theater() {
+		
+	}
+	
+	public Theater (String cosId, String cosPw) {
+		this.cosId = cosId;
+		this.cosPw = cosPw;
+	}
+
+	public String getSysId() {
+		return sysId;
+	}
+
+	public String getSysPw() {
+		return sysPw;
+	}
+
+	public String getCosId() {
+		return cosId;
+	}
+
+	public void setCosId(String cosId) {
+		this.cosId = cosId;
+	}
+
+	public String getCosPw() {
+		return cosPw;
+	}
+
+	public void setCosPw(String cosPw) {
+		this.cosPw = cosPw;
+	}
+
+}
