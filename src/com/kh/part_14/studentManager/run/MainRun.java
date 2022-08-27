a6f4d43282eef8771ca41f710ba9c529a49d2455
jackinquantico <jackinquantico@naver.com>
2022-08-27 18:56:47 +0900

    student management program - initial commit

---
+++ src/com/kh/part_14/studentManager/run/MainRun.java
@@ -0,0 +1,14 @@
+package com.kh.part_14.studentManager.run;
+
+import com.kh.part_14.studentManager.view.StudentView;
+
+public class MainRun {
+
+	public static void main(String[] args) {
+		StudentView sv = new StudentView();
+		
+		sv.mainMenu();
+		
+	}
+		
+}
