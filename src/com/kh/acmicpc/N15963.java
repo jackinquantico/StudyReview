a7a2dc6ef65727f68412140acdc86724b033f0e1
jackinquantico <jackinquantico@naver.com>
2022-08-30 14:52:13 +0900

    baekjoon problem solved

---
+++ src/com/kh/acmicpc/N15963.java
@@ -0,0 +1,22 @@
+package com.kh.acmicpc;
+
+import java.util.Scanner;
+
+public class N15963 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		
+		if (n == m) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+
+	}
+
+}
