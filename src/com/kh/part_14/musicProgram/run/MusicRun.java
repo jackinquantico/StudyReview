cee9afa278335e8db90e434b86bf55d908d3e040
jackinquantico <jackinquantico@naver.com>
2022-08-31 12:42:58 +0900

    music program mvc pattern practice - main method

---
+++ src/com/kh/part_14/musicProgram/run/MusicRun.java
@@ -0,0 +1,15 @@
+package com.kh.part_14.musicProgram.run;
+
+import com.kh.part_14.musicProgram.view.MusicView;
+
+public class MusicRun {
+
+	public static void main(String[] args) {
+		
+		MusicView mv = new MusicView();
+		
+		mv.mainMenu();
+
+	}
+
+}
