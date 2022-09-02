55935042d2175df36adaeaf1822ca5fe4ff7e22b
jackinquantico <jackinquantico@naver.com>
2022-09-02 09:20:55 +0900

    baekjoon solved

---
+++ src/com/kh/acmicpc/N14581.java
@@ -0,0 +1,18 @@
+package com.kh.acmicpc;
+
+import java.util.Scanner;
+
+public class N14581 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.nextLine();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::"+id+"::fan:");
+		System.out.println(":fan::fan::fan:");
+		
+	}
+
+}
