295a5756c1d1482e9217c82de2e26d83bec8fddc
jackinquantico <jackinquantico@naver.com>
2022-09-06 09:17:24 +0900

    before using JDBC - run class

---
+++ src/com/kh/part_14/beforeJDBC/run/Run.java
@@ -0,0 +1,15 @@
+package com.kh.part_14.beforeJDBC.run;
+
+import com.kh.part_14.beforeJDBC.view.MemberView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		MemberView mv = new MemberView();
+		
+		mv.mainMenu();
+
+	}
+
+}
