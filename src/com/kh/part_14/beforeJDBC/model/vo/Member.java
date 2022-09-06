fc077b4ebf9d438f46ecce0018794f94464f1666
jackinquantico <jackinquantico@naver.com>
2022-09-06 09:17:15 +0900

    before using JDBC - vo class

---
+++ src/com/kh/part_14/beforeJDBC/model/vo/Member.java
@@ -0,0 +1,101 @@
+package com.kh.part_14.beforeJDBC.model.vo;
+
+public class Member {
+	
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private int age;
+	private char gender;
+	private String email;
+	private String phone;
+	
+	public Member() {
+		
+	}
+
+	public Member(int userNo, String userId, String userPwd, String userName, int age, char gender, String email,
+			String phone) {
+		super();
+		this.userNo = userNo;
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.age = age;
+		this.gender = gender;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "[Member userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
+				+ ", age=" + age + ", gender=" + gender + ", email=" + email + ", phone=" + phone + "]";
+	}
+		
+}
