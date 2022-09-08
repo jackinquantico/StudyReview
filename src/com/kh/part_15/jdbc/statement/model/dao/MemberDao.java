a5849e377ef40588f4995c2cd8dbeda4a89e0b7c
jackinquantico <jackinquantico@naver.com>
2022-09-08 10:08:09 +0900

    Create MemberDao.java

---
+++ src/com/kh/part_15/jdbc/statement/model/dao/MemberDao.java
@@ -0,0 +1,15 @@
+package com.kh.part_15.jdbc.statement.model.dao;
+
+import com.kh.part_15.jdbc.statement.model.vo.Member;
+
+public class MemberDao {
+
+	public int insertMember(Member m) {
+		
+		// 0. 변수 세팅
+		int result = 0;
+		
+		
+		return result;
+	}
+}
