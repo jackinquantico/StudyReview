46ba4e87103f5148ff241d374451c46c1914f1e2
jackinquantico <jackinquantico@naver.com>
2022-09-09 22:13:30 +0900

    Update Run.java

--- src/com/kh/part_15/jdbc/statement/run/Run.java
+++ src/com/kh/part_15/jdbc/statement/run/Run.java
@@ -1,8 +1,13 @@
 package com.kh.part_15.jdbc.statement.run;
 
+import com.kh.part_15.jdbc.statement.view.MemberView;
+
 public class Run {
 
 	public static void main(String[] args) {
+	
+		MemberView mv = new MemberView();
 		
+		mv.mainMenu();
 	}
 }
