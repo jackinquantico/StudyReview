24033d93406b1bf483ee1c83355089abf17f7b3c
jackinquantico <jackinquantico@naver.com>
2022-09-11 21:00:06 +0900

    Create PreRun.java

---
+++ src/com/kh/part_15/jdbc/preparedStatement/run/PreRun.java
@@ -0,0 +1,13 @@
+package com.kh.part_15.jdbc.preparedStatement.run;
+
+import com.kh.part_15.jdbc.preparedStatement.view.PreView;
+
+public class PreRun {
+
+	public static void main(String[] args) {
+		
+		new PreView().mainMenu();
+
+	}
+
+}
