787ab77748e8b2d267677dde9afa4d2fa4f9d288
jackinquantico <jackinquantico@naver.com>
2022-09-16 13:12:08 +0900

    Create Run.java

---
+++ src/com/kh/part_15/jdbc/properties/run/Run.java
@@ -0,0 +1,17 @@
+package com.kh.part_15.jdbc.properties.run;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.kh.part_15.jdbc.properties.view.MemberView;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		new MemberView().mainMenu();
+		
+	}
+
+}
