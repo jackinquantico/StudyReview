7048dbf54ebf20d6641daeedc3e4e15628ee9f9e
jackinquantico <jackinquantico@naver.com>
2022-09-18 10:24:19 +0900

    Create BoardRun.java

---
+++ src/com/kh/part_15/jdbc/properties/run/BoardRun.java
@@ -0,0 +1,13 @@
+package com.kh.part_15.jdbc.properties.run;
+
+import com.kh.part_15.jdbc.properties.view.BoardView;
+
+public class BoardRun {
+	
+	public static void main(String[] args) {
+		
+		new BoardView().mainMenu();
+	}
+
+	
+}
