916ed7ab8a7eff5710601115a7eb5cc648d91f3a
jackinquantico <jackinquantico@naver.com>
2022-09-27 13:51:25 +0900

    Create Run.java

---
+++ src/com/kh/part_16_JDBC_MiniProject_Total/src/com/kh/run/Run.java
@@ -0,0 +1,15 @@
+package com.kh.run;
+
+import com.kh.view.ProductView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+//		new ProductView().mainMenu();
+		
+//		new ProductView().adminLogin();
+		
+		new ProductView().loginMenu();
+	}
+}
